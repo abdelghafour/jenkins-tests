de9a0776f1e04cd7c0bb5cb3a3b00f78a8bcbace
Abdelghafour Harraz <h.abdelghafour@gmail.com>
2018-05-14 10:31:02 +0000

    testing..

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-// make bluebird default Promise
-Promise = require('bluebird'); // eslint-disable-line no-global-assign
-const lib = require('./lib');
+const express = require("express");
+const utils = require("./controllers/utils");
+const app = express();
 
-const app = lib.initApp();
+const envOne = process.env.VARIABLE_1;
+const envTwo = process.env.VARIABLE_2;
 
-module.exports = app;
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+app.get("/add", function(req, res) {
+  res.send(utils.add(envOne, envTwo));
+});
+
+app.get("/divide", function(req, res) {
+  res.send(utils.divide(envOne, envTwo));
+});
+
+app.get("/multiply", function(req, res) {
+  res.send(utils.multiply(envOne, envTwo));
+});
+
+app.get("/subtract", function(req, res) {
+  res.send(utils.subtract(envOne, envTwo));
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
